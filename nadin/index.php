6bab06f417ca9d943fb5a9220dd101485f9f82ff
nosyrev-13 <76430215+nosyrev-13@users.noreply.github.com>
2021-01-08 23:27:18 +0300

    Add files via upload

---
+++ nadin/index.php
@@ -0,0 +1,100 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Безымянная страница</title>
+<meta name="generator" content="WYSIWYG Web Builder 16 - https://www.wysiwygwebbuilder.com">
+<link href="css/font-awesome.min.css" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Ubuntu:400,700,500&subset=cyrillic" rel="stylesheet">
+<link href="css/nadin.css" rel="stylesheet">
+<link href="css/index.css" rel="stylesheet">
+<script src="js/jquery-1.12.4.min.js"></script>
+<script src="js/jquery-ui.min.js"></script>
+<script src="js/wb.carousel.effects.min.js"></script>
+<script>
+$(document).ready(function()
+{
+   var Carousel1Opts =
+   {
+      delay: 3000,
+      duration: 500,
+      easing: 'linear',
+      mode: 'fade',
+      direction: '',
+      pagination: false,
+      pause: null,
+      start: 0
+   };
+   $("#Carousel1").carouseleffects(Carousel1Opts);
+});
+</script>
+<link href="style.css" rel="stylesheet">
+</head>
+<body>
+<div id="Layer1" style="position:relative;text-align:center;width:100%;height:709px;float:left;clear:left;display:block;z-index:29;">
+<div id="Layer1_Container" style="width:960px;position:relative;margin-left:auto;margin-right:auto;text-align:left;">
+<div id="wb_Image1" style="position:absolute;left:10px;top:10px;width:90px;height:105px;z-index:5;">
+<a href="./index.php"><img src="images/logoo2.png" id="Image1" alt=""></a></div>
+<div id="wb_Text1" style="position:absolute;left:121px;top:47px;width:250px;height:30px;text-align:center;z-index:6;">
+<span style="color:#FFFFFF;font-family:Ubuntu;font-size:13px;">&#1042;&#1077;&#1076;&#1091;&#1097;&#1080;&#1081; &#1073;&#1077;&#1083;&#1086;&#1088;&#1091;&#1089;&#1089;&#1082;&#1080;&#1081; &#1087;&#1088;&#1086;&#1080;&#1079;&#1074;&#1086;&#1076;&#1080;&#1090;&#1077;&#1083;&#1100; &#1078;&#1077;&#1085;&#1089;&#1082;&#1086;&#1081; &#1086;&#1076;&#1077;&#1078;&#1076;&#1099; &#1073;&#1086;&#1083;&#1100;&#1096;&#1080;&#1093; &#1088;&#1072;&#1079;&#1084;&#1077;&#1088;&#1086;&#1074;</span></div>
+<div id="wb_Text2" style="position:absolute;left:0px;top:147px;width:511px;height:215px;z-index:7;">
+<span style="color:#FFFFFF;font-family:'Ubuntu';font-weight:700;font-size:48px;">&#1046;&#1077;&#1085;&#1089;&#1082;&#1072;&#1103; &#1086;&#1076;&#1077;&#1078;&#1076;&#1072; &#1086;&#1087;&#1090;&#1086;&#1084; 42-68 &#1088;&#1072;&#1079;&#1084;&#1077;&#1088;&#1086;&#1074; &#1085;&#1072;&#1087;&#1088;&#1103;&#1084;&#1091;&#1102;<br>&#1086;&#1090; &#1087;&#1088;&#1086;&#1080;&#1079;&#1074;&#1086;&#1076;&#1080;&#1090;&#1077;&#1083;&#1103;</span></div>
+<div id="wb_Text3" style="position:absolute;left:106px;top:398px;width:270px;height:18px;z-index:8;">
+<span style="color:#FFFFFF;font-family:Ubuntu;font-size:16px;">&#1052;&#1080;&#1085;&#1080;&#1084;&#1072;&#1083;&#1100;&#1085;&#1099;&#1081; &#1079;&#1072;&#1082;&#1072;&#1079; &#1086;&#1090; 10 &#1077;&#1076;&#1080;&#1085;&#1080;&#1094;</span></div>
+<div id="wb_Text4" style="position:absolute;left:106px;top:428px;width:370px;height:18px;z-index:9;">
+<span style="color:#FFFFFF;font-family:Ubuntu;font-size:16px;">&#1053;&#1072;&#1087;&#1088;&#1103;&#1084;&#1091;&#1102; &#1086;&#1090; &#1087;&#1088;&#1086;&#1080;&#1079;&#1074;&#1086;&#1076;&#1080;&#1090;&#1077;&#1083;&#1103; (&#1073;&#1077;&#1079; &#1087;&#1086;&#1089;&#1088;&#1077;&#1076;&#1085;&#1080;&#1082;&#1086;&#1074;)</span></div>
+<div id="wb_Text5" style="position:absolute;left:106px;top:458px;width:370px;height:18px;z-index:10;">
+<span style="color:#FFFFFF;font-family:Ubuntu;font-size:16px;">&#1044;&#1086;&#1089;&#1090;&#1072;&#1074;&#1082;&#1072; &#1087;&#1086; &#1074;&#1089;&#1077;&#1084; &#1089;&#1090;&#1088;&#1072;&#1085;&#1072;&#1084; &#1057;&#1053;&#1043; </span></div>
+<div id="wb_IconFont1" style="position:absolute;left:74px;top:396px;width:24px;height:22px;text-align:center;z-index:11;">
+<div id="IconFont1"><i class="IconFont1"></i></div></div>
+<div id="wb_IconFont2" style="position:absolute;left:74px;top:426px;width:24px;height:22px;text-align:center;z-index:12;">
+<div id="IconFont2"><i class="IconFont2"></i></div></div>
+<div id="wb_IconFont3" style="position:absolute;left:74px;top:456px;width:24px;height:22px;text-align:center;z-index:13;">
+<div id="IconFont3"><i class="IconFont3"></i></div></div>
+<div id="wb_Image2" style="position:absolute;left:36px;top:478px;width:64px;height:64px;z-index:14;">
+<img src="images/share (1).png" id="Image2" alt=""></div>
+<div id="wb_Text6" style="position:absolute;left:111px;top:579px;width:302px;height:18px;text-align:center;z-index:15;">
+<span style="color:#FFFFFF;font-family:Ubuntu;font-size:16px;">или</span></div>
+<div id="wb_Text7" style="position:absolute;left:111px;top:608px;width:302px;height:36px;text-align:center;z-index:16;">
+<span style="color:#DB7093;font-family:Arial;font-size:16px;"><u>&#1055;&#1086;&#1083;&#1091;&#1095;&#1080;&#1090;&#1077; &#1087;&#1088;&#1077;&#1076;&#1083;&#1086;&#1078;&#1077;&#1085;&#1080;&#1077; &#1086; &#1089;&#1086;&#1090;&#1088;&#1091;&#1076;&#1085;&#1080;&#1095;&#1077;&#1089;&#1090;&#1074;&#1077;</u></span></div>
+<div id="wb_Image3" style="position:absolute;left:335px;top:580px;width:200px;height:92px;z-index:17;">
+<img src="images/gotovyij-prajs5.png" id="Image3" alt=""></div>
+<div id="wb_Carousel1" style="position:absolute;left:565px;top:57px;width:395px;height:649px;z-index:18;overflow:hidden;">
+<div id="Carousel1" style="position:absolute;">
+<div class="frame">
+<div id="wb_Image4" style="position:absolute;left:2px;top:0px;width:367px;height:645px;z-index:0;">
+<img src="images/bg3.jpg" id="Image4" alt=""></div>
+</div>
+<div class="frame" style="display:none">
+<div id="wb_Image5" style="position:absolute;left:3px;top:3px;width:372px;height:642px;z-index:1;">
+<img src="images/bg4.jpg" id="Image5" alt=""></div>
+</div>
+<div class="frame" style="display:none">
+<div id="wb_Image6" style="position:absolute;left:0px;top:0px;width:367px;height:645px;z-index:2;">
+<img src="images/bg5.jpg" id="Image6" alt=""></div>
+</div>
+<div class="frame" style="display:none">
+<div id="wb_Image7" style="position:absolute;left:0px;top:0px;width:367px;height:645px;z-index:3;">
+<img src="images/bg11.jpg" id="Image7" alt=""></div>
+</div>
+<div class="frame" style="display:none">
+<div id="wb_Image8" style="position:absolute;left:0px;top:0px;width:368px;height:646px;z-index:4;">
+<img src="images/bg22.jpg" id="Image8" alt=""></div>
+</div>
+</div>
+</div>
+<div id="wb_Text8" style="position:absolute;left:805px;top:10px;width:128px;height:21px;text-align:center;z-index:19;">
+<span style="color:#000000;font-family:Ubuntu;font-size:16px;">+795 </span><span style="color:#000000;font-family:Ubuntu;font-size:19px;"><strong>85825685</strong></span></div>
+<div id="wb_Text9" style="position:absolute;left:805px;top:33px;width:128px;height:18px;text-align:center;z-index:20;cursor:pointer;">
+<span style="color:#DB7093;font-family:'Ubuntu';font-weight:500;font-size:16px;line-height:22px;"><u>Заказать звонок</u></span></div>
+<div id="wb_Image9" style="position:absolute;left:770px;top:8px;width:24px;height:24px;z-index:21;">
+<img src="images/phone-call (3).png" id="Image9" alt=""></div>
+<div id="wb_Shape1" style="position:absolute;left:120px;top:494px;width:285px;height:66px;z-index:22;">
+<a href="./index.php"><div id="Shape1"><div id="Shape1_text"><span style="color:#FFFFFF;font-family:Ubuntu;font-size:19px;">Перейти в каталог </span><span style="color:#FFFFFF;font-family:'Cambria Math';font-size:16px;">&#8594;</span></div></div></a></div>
+<div id="wb_IconFont4" style="position:absolute;left:132px;top:512px;width:30px;height:30px;text-align:center;z-index:23;">
+<div id="IconFont4"><i class="fa fa-play-circle"></i></div></div>
+</div>
+</div>
+
+</body>
+</html>
